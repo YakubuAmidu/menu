479a5f0ae63318e7c27b7028a7c7a8cb4a5bfdca
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-18 21:28:08 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -75,8 +75,18 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// load items
 window.addEventListener('DOMContentLoaded', function () {
   displayMenuItem(menu);
+});
+
+// filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    console.log(e.currentTarget.dataset);
+  });
 });
 
 function displayMenuItem(menuItems) {
