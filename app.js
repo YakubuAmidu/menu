139249585ef01f682b5ebafc4ebb86bc878af2a5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-18 19:39:14 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function () {
   let displayMenu = menu.map(function (item) {
-    console.log(item);
+    return item;
   });
+  console.log(displayMenu);
 });
